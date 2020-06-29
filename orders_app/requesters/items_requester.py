c3be81819633627b638a0271d68c5ca88f303dce
Irina <irinaadamova555@gmail.com>
2020-06-30 00:47:10 +0300

    correct urls

--- orders_app/requesters/items_requester.py
+++ orders_app/requesters/items_requester.py
@@ -2,7 +2,8 @@ from orders_app.requesters.requester import Requester
 
 
 class ItemsRequester(Requester):
-    ITEMS_HOST = Requester.HOST + ':8001/'
+    #ITEMS_HOST = Requester.HOST + ':8001/'
+    ITEMS_HOST = 'https://rsoi-online-store-items.herokuapp.com/'
 
     def get_item(self, uuid):
         response = self.get_request(self.ITEMS_HOST + str(uuid) + '/')
